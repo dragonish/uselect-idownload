0f3f701d1ecd7d30a59867f7e02c2a122f4fb49a
dragonish <no.web.developer@outlook.com>
2024-03-02 21:31:33 +0800

    fix(backgroud): fix cannot open tabs in the background

--- js/background.js
+++ js/background.js
@@ -21,72 +21,73 @@
  */
 
 function req_action_handler(request, sender, sendResponse) {
-	var urls = request['urls'];
-	var action = request['action'];
-	var tabId = request['tabId'];
+  var urls = request['urls'];
+  var action = request['action'];
+  var tabId = request['tabId'];
 
-	if (action == 'window') {
-		/*
-		 * TODO:
-		 * - incognito window
-		 * - selected
-		 */
-		chrome.windows.create({'url': urls});
-	} else {
-		switch (action) {
-		case 'download':
-			urls.forEach(function (url) {
-				console.log(url);
-				chrome.downloads.download({
-					'url': url,
-				});
-			});
-			chrome.tabs.create({
-				'url': 'chrome://downloads',
-				'selected': true,
-				'openerTabId': sender.tab.id,
-			});
-			break;
-		case 'tabs':
-			urls.forEach(function (url) {
-				chrome.tabs.create({
-					'url': url,
-					'selected': false,
-					'openerTabId': sender.tab.id,
-				});
-			});
-			break;
-		}
-	}
-	// The chrome.extension.onMessage listener must return true if you want to send a response after the listener returns
-	return true;
+  if (action == 'window') {
+    /*
+     * TODO:
+     * - incognito window
+     * - selected
+     */
+    chrome.windows.create({ url: urls });
+  } else {
+    switch (action) {
+      case 'download':
+        urls.forEach(function (url) {
+          console.log(url);
+          chrome.downloads.download({
+            url: url,
+          });
+        });
+        chrome.tabs.create({
+          url: 'chrome://downloads',
+          selected: true,
+          active: true,
+          openerTabId: sender.tab.id,
+        });
+        break;
+      case 'tabs':
+        urls.forEach(function (url) {
+          chrome.tabs.create({
+            url: url,
+            selected: false,
+            active: false,
+            openerTabId: sender.tab.id,
+          });
+        });
+        break;
+    }
+  }
+  // The chrome.extension.onMessage listener must return true if you want to send a response after the listener returns
+  return true;
 }
 
 function req_inject_handler(request, sender, sendResponse) {
-	chrome.tabs.executeScript(sender.tab.id, {file: 'js/statemachine.js'}, function () {
-		chrome.tabs.executeScript(sender.tab.id, {file: 'js/overlay.js'}, function () {
-			if (sendResponse != null)
-				sendResponse();
-		});
-	});
-	// The chrome.extension.onMessage listener must return true if you want to send a response after the listener returns
-	return true;
+  chrome.tabs.executeScript(sender.tab.id, { file: 'js/statemachine.js' }, function () {
+    chrome.tabs.executeScript(sender.tab.id, { file: 'js/overlay.js' }, function () {
+      if (sendResponse != null) sendResponse();
+    });
+  });
+  // The chrome.extension.onMessage listener must return true if you want to send a response after the listener returns
+  return true;
 }
 
 /**
  * Demultiplex requests using the '__req__' property of the request object
  */
 chrome.extension.onMessage.addListener(function (request, sender, sendResponse) {
-	switch (request['__req__']) {
-	case 'inject':
-		return req_inject_handler(request, sender, sendResponse);
-	case 'action':
-		return req_action_handler(request, sender, sendResponse);
-	default:
-		console.log('Unknown message');
-	}
+  switch (request['__req__']) {
+    case 'inject':
+      return req_inject_handler(request, sender, sendResponse);
+    case 'action':
+      return req_action_handler(request, sender, sendResponse);
+    default:
+      console.log('Unknown message');
+  }
 });
 
 chrome.browserAction.onClicked.addListener(function (tab) {
-	chrome.tabs.sendMessage(tab.id, 'toggle');
+  chrome.tabs.sendMessage(tab.id, 'toggle');
 });
